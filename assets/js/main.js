88cf32baa516081b0cd00e799741d68a0329e4b2
Lmsilvano <leandro.m.silvano@gmail.com>
2021-09-16 00:48:03 -0300

    Projeto concluido e comentado
    Falta o readme

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,23 @@ window.onload = function () {
 }
 
 // 1. Função de entrada de dados (disparada no botão de adcionar tarefas). 
-// 1.2 Adiciona tarefas, coletadas no input, ao Array de tarefas. Salva dados no local storage, limpa input e atualiza dados impressos na tela.
-
+// Adiciona tarefas, coletadas no input, ao Array de tarefas. Salva dados no local storage, limpa input, atualiza dados impressos na tela
+// e retorna mensagem de erro caso usuário tente enviar tarefa em branco.
 
 
 function sendTask() {
 
     document.querySelector('.contentWarn').innerHTML = '';
     const tasksInputData = document.querySelector('#taskInput');
+    if (tasksInputData.value === '') {
+        let warning = document.querySelector('.contentWarn')
+        warning.style = 'display: block;'
+        warning.innerText = `Impossível adicionar tarefa em branco.`
+        setTimeout(function () {
+            warning.style = 'display: none;'
+        }, 1450);
+        return;
+    }
     printTasksOnClick(tasksInputData.value);
     salvedata();
     clearInput(tasksInputData);
@@ -20,12 +29,19 @@ function sendTask() {
 
 };
 
+// 1.2 dispara função acima com ao usuário pressionar tecla enter no input de tarefas
+
+document.querySelector('#taskInput').addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        sendTask()
+    };
+});
 
 
 // 2. Funções
 
 
-
+//2.1 Função construtora de elementos do DOM.
 function DOMConstructor(elem, elem2, elem3, attribute) {
 
     let element = document.createElement(elem);
@@ -33,20 +49,20 @@ function DOMConstructor(elem, elem2, elem3, attribute) {
     return elem3 === '.m-content_C' ? document.querySelector(elem3).appendChild(element) : elem3.appendChild(element)
 }
 
-
+//2.2 Função para limpar input
 function clearInput(input) {
     input.value = '';
 }
 
+//2.3 Função para salvar dados no localStorage (retirados da HTML), e atualizar marcação ou remoção dos dados no localStorage.
 
 function salvedata() {
 
     let tasksList = document.querySelectorAll('.c-tasks__label');
     let tasksArray = [];
     for (let task of tasksList) {
-        let taskText = {name:task.innerText, markup: 0};
-        if(task.parentElement.id === 'checked'){
-            console.log('entrei no console log');
+        let taskText = { name: task.innerText, markup: 0 };
+        if (task.parentElement.id === 'checked') {
             taskText.markup = 1;
         }
         tasksArray.push(taskText);
@@ -56,7 +72,7 @@ function salvedata() {
     localStorage.setItem('TasksData', tasksJSON)
 };
 
-
+//2.4 função para imprimir dados no HTML após o clique do usuário.
 
 function printTasksOnClick(argument) {
     let divContentTask = DOMConstructor("div", "m-content__tasks", ".m-content_C", 'class');
@@ -69,8 +85,7 @@ function printTasksOnClick(argument) {
     btnRemoveTask.innerText = 'Remover Tarefa';
 }
 
-
-
+//2.5 função para imprimir dados do localStorage após o carregamento da página, se não houver dados retorna mensagem.
 
 function printTasksOnload() {
     const tasksFromlocal = JSON.parse(localStorage.getItem('TasksData')) || [];
@@ -81,8 +96,13 @@ function printTasksOnload() {
 
             let divContentTask = DOMConstructor("div", "m-content__tasks", ".m-content_C", 'class');
             let inputCheckBox = DOMConstructor("input", "checkbox", divContentTask, 'type');
-            inputCheckBox.setAttribute('class', 'c-tasks__checkbox')
-            inputCheckBox.setAttribute('id', 'checkboxTask')
+            inputCheckBox.setAttribute('class', 'c-tasks__checkbox');
+            inputCheckBox.setAttribute('id', 'checkboxTask');
+            // 2.5.6 se tarefa foi marcada recebe 1 no atributo markup
+            if (task.markup === 1) {
+                divContentTask.setAttribute('id', 'checked');
+                inputCheckBox.checked = true;
+            }
             let labelForCheckbox = DOMConstructor("label", "c-tasks__label", divContentTask, 'class');
             labelForCheckbox.innerText = task.name;
             let btnRemoveTask = DOMConstructor("button", "c-tasks__btnRemove", divContentTask, 'class');
@@ -94,23 +114,42 @@ function printTasksOnload() {
     }
 }
 
+// 3. Eventos de click
 
 document.addEventListener('click', (e) => {
 
     let el = e.target
-    
+    //3.1 Callback para deletar tarefa da tela e do localStorage ao clicar no botão de remover tarefa.
     if (el.classList.contains('c-tasks__btnRemove')) {
-        el.parentElement.remove()
-        salvedata()
+        if (window.confirm("Deseja excluir a tarefa?")) {
+            el.parentElement.remove()
+            salvedata();
+        } else {
+            return;
+        }
     };
+    //3.2 Callback configurado no clique no checkbox ou no label para marcar ou desmarcar tarefa e Atualizar localStorage.
+    //3.2.1 no Label
     if (el.classList.contains('c-tasks__label')) {
+        if (el.parentElement.id === 'checked') {
+            let sibling = el.previousElementSibling;
+            sibling.checked = false;
+            el.parentElement.removeAttribute('id')
+            salvedata();
+            return;
+        }
         let sibling = el.previousElementSibling;
         sibling.checked = true;
         el.parentElement.setAttribute('id', 'checked')
         salvedata();
     };
-
+    //3.2.2 no Checkbox
     if (el.id === 'checkboxTask') {
+        if (el.parentElement.id === 'checked') {
+            el.parentElement.removeAttribute('id')
+            salvedata();
+            return;
+        }
         el.parentElement.setAttribute('id', 'checked')
         salvedata();
 
